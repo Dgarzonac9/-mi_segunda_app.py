6cfce7211d9f80a934a37acdd03527bb81546c47
Dgarzonac9 <142264577+Dgarzonac9@users.noreply.github.com>
2023-08-28 19:12:05 -0500

    Update conversor_datos.py
    cambio 4f a 2f

--- conversor_datos.py
+++ conversor_datos.py
@@ -208,23 +208,23 @@ elif categoria_seleccionada == "Longitud":
 
     unidad_origen, unidad_destino = conversion_seleccionada.split(" a ")
     resultado = convertir_longitud(valor, unidad_origen, unidad_destino)
-    st.write(f"{valor} {unidad_origen} son {resultado:.4f} {unidad_destino}")
+    st.write(f"{valor} {unidad_origen} son {resultado:.2f} {unidad_destino}")
 
     if conversion_seleccionada == "Pies a metros":
         resultado = convertir_longitud(valor, "pies", "metros")
-        st.write(f"{valor} pies son {resultado:.4f} metros")
+        st.write(f"{valor} pies son {resultado:.2f} metros")
 
     elif conversion_seleccionada == "Metros a pies":
         resultado = convertir_longitud(valor, "metros", "pies")
-        st.write(f"{valor} metros son {resultado:.4f} pies")
+        st.write(f"{valor} metros son {resultado:.2f} pies")
 
     elif conversion_seleccionada == "Pulgadas a centímetros":
         resultado = convertir_longitud(valor, "pulgadas", "metros") * 100
-        st.write(f"{valor} pulgadas son {resultado:.4f} centímetros")
+        st.write(f"{valor} pulgadas son {resultado:.2f} centímetros")
 
     elif conversion_seleccionada == "Centímetros a pulgadas":
         resultado = convertir_longitud(valor, "metros", "pulgadas") / 100
-        st.write(f"{valor} centímetros son {resultado:.4f} pulgadas")
+        st.write(f"{valor} centímetros son {resultado:.2f} pulgadas")
 
     pass
 
@@ -235,26 +235,26 @@ elif categoria_seleccionada == "Peso/Masa":
 
     unidad_origen, unidad_destino = conversion_seleccionada.split(" a ")
     resultado = convertir_peso(valor, unidad_origen, unidad_destino)
-    st.write(f"{valor} {unidad_origen} son {resultado:.4f} {unidad_destino}")
-
+    st.write(f"{valor} {unidad_origen} son {resultado:.2f} {unidad_destino}")
 
     if conversion_seleccionada == "Libras a kilogramos":
         resultado = convertir_peso(valor, "libras", "kilogramos")
-        st.write(f"{valor} libras son {resultado:.4f} kilogramos")
+        st.write(f"{valor} libras son {resultado:.2f} kilogramos")
 
     elif conversion_seleccionada == "Kilogramos a libras":
         resultado = convertir_peso(valor, "kilogramos", "libras")
-        st.write(f"{valor} kilogramos son {resultado:.4f} libras")
+        st.write(f"{valor} kilogramos son {resultado:.2f} libras")
 
     elif conversion_seleccionada == "Onzas a gramos":
         resultado = convertir_peso(valor, "onzas", "gramos")
-        st.write(f"{valor} onzas son {resultado:.4f} gramos")
+        st.write(f"{valor} onzas son {resultado:.2f} gramos")
 
     elif conversion_seleccionada == "Gramos a onzas":
         resultado = convertir_peso(valor, "gramos", "onzas")
-        st.write(f"{valor} gramos son {resultado:.4f} onzas")
+        st.write(f"{valor} gramos son {resultado:.2f} onzas")
 
     pass
+
 
 elif categoria_seleccionada == "Volumen":
     conversiones_volumen = ["Galones a litros", "Litros a galones", "Pulgadas cúbicas a centímetros cúbicos", "Centímetros cúbicos a pulgadas cúbicas"]
@@ -263,26 +263,26 @@ elif categoria_seleccionada == "Volumen":
 
     unidad_origen, unidad_destino = conversion_seleccionada.split(" a ")
     resultado = convertir_volumen(valor, unidad_origen, unidad_destino)
-    st.write(f"{valor} {unidad_origen} son {resultado:.4f} {unidad_destino}")
-
+    st.write(f"{valor} {unidad_origen} son {resultado:.2f} {unidad_destino}")
 
     if conversion_seleccionada == "Galones a litros":
         resultado = convertir_volumen(valor, "galones", "litros")
-        st.write(f"{valor} galones son {resultado:.4f} litros")
+        st.write(f"{valor} galones son {resultado:.2f} litros")
 
     elif conversion_seleccionada == "Litros a galones":
         resultado = convertir_volumen(valor, "litros", "galones")
-        st.write(f"{valor} litros son {resultado:.4f} galones")
+        st.write(f"{valor} litros son {resultado:.2f} galones")
 
     elif conversion_seleccionada == "Pulgadas cúbicas a centímetros cúbicos":
         resultado = convertir_volumen(valor, "pulgadas cúbicas", "metros cúbicos") * 1e6
-        st.write(f"{valor} pulgadas cúbicas son {resultado:.4f} centímetros cúbicos")
+        st.write(f"{valor} pulgadas cúbicas son {resultado:.2f} centímetros cúbicos")
 
     elif conversion_seleccionada == "Centímetros cúbicos a pulgadas cúbicas":
         resultado = convertir_volumen(valor, "metros cúbicos", "pulgadas cúbicas") * 1e-6
-        st.write(f"{valor} centímetros cúbicos son {resultado:.4f} pulgadas cúbicas")
+        st.write(f"{valor} centímetros cúbicos son {resultado:.2f} pulgadas cúbicas")
 
     pass
+
 
 elif categoria_seleccionada == "Tiempo":
     conversiones_tiempo = ["Horas a minutos", "Minutos a segundos", "Días a horas", "Semanas a días"]
@@ -291,24 +291,23 @@ elif categoria_seleccionada == "Tiempo":
 
     unidad_origen, unidad_destino = conversion_seleccionada.split(" a ")
     resultado = convertir_tiempo(valor, unidad_origen, unidad_destino)
-    st.write(f"{valor} {unidad_origen} son {resultado:.4f} {unidad_destino}")
-
+    st.write(f"{valor} {unidad_origen} son {resultado:.2f} {unidad_destino}")
 
     if conversion_seleccionada == "Horas a minutos":
-        resultado = valor * 60
-        st.write(f"{valor} horas son {resultado:.4f} minutos")
+        resultado = convertir_tiempo(valor, "Horas", "Minutos")
+        st.write(f"{valor} horas son {resultado:.2f} minutos")
 
     elif conversion_seleccionada == "Minutos a segundos":
-        resultado = valor * 60
-        st.write(f"{valor} minutos son {resultado:.4f} segundos")
+        resultado = convertir_tiempo(valor, "Minutos", "Segundos")
+        st.write(f"{valor} minutos son {resultado:.2f} segundos")
 
     elif conversion_seleccionada == "Días a horas":
-        resultado = valor * 24
-        st.write(f"{valor} días son {resultado:.4f} horas")
+        resultado = convertir_tiempo(valor, "Días", "Horas")
+        st.write(f"{valor} días son {resultado:.2f} horas")
 
     elif conversion_seleccionada == "Semanas a días":
-        resultado = valor * 7
-        st.write(f"{valor} semanas son {resultado:.4f} días")
+        resultado = convertir_tiempo(valor, "Semanas", "Días")
+        st.write(f"{valor} semanas son {resultado:.2f} días")
 
     pass
 
@@ -319,52 +318,24 @@ elif categoria_seleccionada == "Velocidad":
 
     unidad_origen, unidad_destino = conversion_seleccionada.split(" a ")
     resultado = convertir_velocidad(valor, unidad_origen, unidad_destino)
-    st.write(f"{valor} {unidad_origen} son {resultado:.4f} {unidad_destino}")
+    st.write(f"{valor} {unidad_origen} son {resultado:.2f} {unidad_destino}")
 
 
     if conversion_seleccionada == "Millas por hora a kilómetros por hora":
         resultado = valor * 1.60934
-        st.write(f"{valor} millas por hora son {resultado:.4f} kilómetros por hora")
+        st.write(f"{valor} millas por hora son {resultado:.2f} kilómetros por hora")
 
     elif conversion_seleccionada == "Kilómetros por hora a metros por segundo":
         resultado = valor * 0.277778
-        st.write(f"{valor} kilómetros por hora son {resultado:.4f} metros por segundo")
+        st.write(f"{valor} kilómetros por hora son {resultado:.2f} metros por segundo")
 
     elif conversion_seleccionada == "Nudos a millas por hora":
         resultado = valor * 1.15078
-        st.write(f"{valor} nudos son {resultado:.4f} millas por hora")
+        st.write(f"{valor} nudos son {resultado:.2f} millas por hora")
 
     elif conversion_seleccionada == "Metros por segundo a pies por segundo":
         resultado = valor * 3.28084
-        st.write(f"{valor} metros por segundo son {resultado:.4f} pies por segundo")
-
-    pass
-
-elif categoria_seleccionada == "Velocidad":
-    conversiones_velocidad = ["Millas por hora a kilómetros por hora", "Kilómetros por hora a metros por segundo", "Nudos a millas por hora", "Metros por segundo a pies por segundo"]
-    conversion_seleccionada = st.sidebar.selectbox("Tipo de conversión", conversiones_velocidad)
-    valor = st.number_input("Ingresa el valor", value=0.0)
-
-    unidad_origen, unidad_destino = conversion_seleccionada.split(" a ")
-    resultado = convertir_velocidad(valor, unidad_origen, unidad_destino)
-    st.write(f"{valor} {unidad_origen} son {resultado:.4f} {unidad_destino}")
-
-
-    if conversion_seleccionada == "Metros cuadrados a pies cuadrados":
-        resultado = valor * 10.7639
-        st.write(f"{valor} metros cuadrados son {resultado:.4f} pies cuadrados")
-
-    elif conversion_seleccionada == "Pies cuadrados a metros cuadrados":
-        resultado = valor * 0.092903
-        st.write(f"{valor} pies cuadrados son {resultado:.4f} metros cuadrados")
-
-    elif conversion_seleccionada == "Kilómetros cuadrados a millas cuadradas":
-        resultado = valor * 0.386102
-        st.write(f"{valor} kilómetros cuadrados son {resultado:.4f} millas cuadradas")
-
-    elif conversion_seleccionada == "Millas cuadradas a kilómetros cuadrados":
-        resultado = valor * 2.58999
-        st.write(f"{valor} millas cuadradas son {resultado:.4f} kilómetros cuadrados")
+        st.write(f"{valor} metros por segundo son {resultado:.2f} pies por segundo")
 
     pass
 
@@ -375,24 +346,24 @@ elif categoria_seleccionada == "Energía":
 
     unidad_origen, unidad_destino = conversion_seleccionada.split(" a ")
     resultado = convertir_energia(valor, unidad_origen, unidad_destino)
-    st.write(f"{valor} {unidad_origen} son {resultado:.4f} {unidad_destino}")
+    st.write(f"{valor} {unidad_origen} son {resultado:.2f} {unidad_destino}")
 
 
     if conversion_seleccionada == "Julios a calorías":
         resultado = valor * 0.239006
-        st.write(f"{valor} julios son {resultado:.4f} calorías")
+        st.write(f"{valor} julios son {resultado:.2f} calorías")
 
     elif conversion_seleccionada == "Calorías a kilojulios":
         resultado = valor * 0.004184
-        st.write(f"{valor} calorías son {resultado:.4f} kilojulios")
+        st.write(f"{valor} calorías son {resultado:.2f} kilojulios")
 
     elif conversion_seleccionada == "Kilovatios-hora a megajulios":
         resultado = valor * 3.6
-        st.write(f"{valor} kilovatios-hora son {resultado:.4f} megajulios")
+        st.write(f"{valor} kilovatios-hora son {resultado:.2f} megajulios")
 
     elif conversion_seleccionada == "Megajulios a kilovatios-hora":
         resultado = valor * 0.277778
-        st.write(f"{valor} megajulios son {resultado:.4f} kilovatios-hora")
+        st.write(f"{valor} megajulios son {resultado:.2f} kilovatios-hora")
 
     pass
 
@@ -403,7 +374,7 @@ elif categoria_seleccionada == "Presión":
 
     unidad_origen, unidad_destino = conversion_seleccionada.split(" a ")
     resultado = convertir_presion(valor, unidad_origen, unidad_destino)
-    st.write(f"{valor} {unidad_origen} son {resultado:.4f} {unidad_destino}")
+    st.write(f"{valor} {unidad_origen} son {resultado:.2f} {unidad_destino}")
 
 
     if conversion_seleccionada == "Pascales a atmósferas":
@@ -412,15 +383,15 @@ elif categoria_seleccionada == "Presión":
 
     elif conversion_seleccionada == "Atmósferas a pascales":
         resultado = valor * 101325
-        st.write(f"{valor} atmósferas son {resultado:.4f} pascales")
+        st.write(f"{valor} atmósferas son {resultado:.2f} pascales")
 
     elif conversion_seleccionada == "Barras a libras por pulgada cuadrada":
         resultado = valor * 14.5038
-        st.write(f"{valor} barras son {resultado:.4f} libras por pulgada cuadrada")
+        st.write(f"{valor} barras son {resultado:.2f} libras por pulgada cuadrada")
 
     elif conversion_seleccionada == "Libras por pulgada cuadrada a bares":
         resultado = valor * 0.0689476
-        st.write(f"{valor} libras por pulgada cuadrada son {resultado:.4f} bares")
+        st.write(f"{valor} libras por pulgada cuadrada son {resultado:.2f} bares")
 
     pass
 
@@ -431,23 +402,23 @@ elif categoria_seleccionada == "Tamaño de Datos":
 
     unidad_origen, unidad_destino = conversion_seleccionada.split(" a ")
     resultado = convertir_tamano_datos(valor, unidad_origen, unidad_destino)
-    st.write(f"{valor} {unidad_origen} son {resultado:.6f} {unidad_destino}")
+    st.write(f"{valor} {unidad_origen} son {resultado:.2f} {unidad_destino}")
 
 
     if conversion_seleccionada == "Megabytes a gigabytes":
         resultado = valor * 0.001
-        st.write(f"{valor} megabytes son {resultado:.6f} gigabytes")
+        st.write(f"{valor} megabytes son {resultado:.2f} gigabytes")
 
     elif conversion_seleccionada == "Gigabytes a terabytes":
         resultado = valor * 0.001
-        st.write(f"{valor} gigabytes son {resultado:.6f} terabytes")
+        st.write(f"{valor} gigabytes son {resultado:.2f} terabytes")
 
     elif conversion_seleccionada == "Kilobytes a megabytes":
         resultado = valor * 0.001
-        st.write(f"{valor} kilobytes son {resultado:.6f} megabytes")
+        st.write(f"{valor} kilobytes son {resultado:.2f} megabytes")
 
     elif conversion_seleccionada == "Terabytes a petabytes":
         resultado = valor * 0.001
-        st.write(f"{valor} terabytes son {resultado:.6f} petabytes")
+        st.write(f"{valor} terabytes son {resultado:.2f} petabytes")
 
     pass
